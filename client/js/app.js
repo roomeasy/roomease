58d1f7e66812f792610f2949e15ae22d929fc1b6
Hadley Lambeth <hlambeth@gmail.com>
2015-07-16 12:17:09 -0500

    Setup angular view controller

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,13 @@
 // Angular initialization
-angular.module('roomEase', [])
-  .controller('MainController', function($scope){
-    $scope.greeting = "Hello"
+angular.module('roomEase', [
+  'ngRoute'
+])
+
+.config(function ($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: '/js/controllers/houseTaskSetup/houseTaskSetup.html',
+    controller: 'houseTaskSetup'
+    // controllerAs: 'taskSetup'
   })
+  .otherwise({redirectTo: '/'})
+})
---
+++ client/js/controllers/houseTaskSetup/houseTaskSetup.js
@@ -0,0 +1,6 @@
+angular.module('roomEase')
+
+.controller('houseTaskSetup', function($scope){
+  $scope.greeting = "Hello";
+
+})
