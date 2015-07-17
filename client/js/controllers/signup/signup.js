4dfec879a34b1308cd7de714fd76824657c63f9d
Abhi Gulati <abhigulati@gmail.com>
2015-07-17 17:29:17 -0500

    changed redirect route

--- client/js/controllers/signup/signup.js
+++ client/js/controllers/signup/signup.js
@@ -11,7 +11,7 @@ angular.module('roomEase')
   $scope.submitSignupInfo = function () {
     Users.addUser($scope.user)
     .then(function() {
-      $location.path('/dashboard');
+      $location.path('/createLivingSpace');
     })
   }
 
