86f1306f1fbf3f41b56d54899f4267b690bafddf
Abhi Gulati <abhigulati@gmail.com>
2015-07-21 14:18:16 -0500

    wrote fn getAllTaskInstances in taskModel

--- server/task/taskModel.js
+++ server/task/taskModel.js
@@ -18,7 +18,21 @@ exports.add = function(task, dwellingId, cb){
 exports.getAll = function(dwellingId, cb){
   var queryString = "SELECT * FROM tasks WHERE dwelling_id = " + dwellingId + ";";
   db.query(queryString, function(err, results){
-    console.log("Inside the getAllTAsks query");
+    console.log("Inside the getAllTasks query");
+    err ? cb(err, null) : cb(null, results.rows);
+  })
+}
+
+exports.getAllInstances = function(dwellingId, cb){
+  var queryString = "SELECT tasks.name, tasks.description, due_date \
+     FROM tasks, task_instances \
+     WHERE task_instances.task_id = tasks.id \
+     AND tasks.dwelling_id = " + dwellingId + ";";
+     
+  db.query(queryString, function(err, results){
+    console.log("Inside the getAllTaskInstances query");
+    console.log("err = ", err);
+    console.log("results = ", results);
     err ? cb(err, null) : cb(null, results.rows);
   })
 }
