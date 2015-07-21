1deb1e419ba07208a6ea7f0490a78ea9b34764e9
Abhi Gulati <abhigulati@gmail.com>
2015-07-21 13:12:34 -0500

    making some test data for tasks

---
+++ server/task_tests.sql
@@ -0,0 +1,5 @@
+INSERT INTO dwellings (address, name) 
+                     VALUES ('123 Other St', 'The House');
+
+INSERT INTO tasks (name, dwelling_id, frequency, description) 
+                     VALUES ('do stuff', 1, 1, 'often');
