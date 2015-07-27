520b607e6aea10664d747e3d3cbf55a0f41505a9
colin-h <colinharman@utexas.edu>
2015-07-27 11:00:48 -0500

    added heroku port

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ function isLoggedIn(req, res, next) {
 }
 
 // Initiate the server
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
