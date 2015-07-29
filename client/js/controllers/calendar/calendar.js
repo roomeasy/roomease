3253cd054c25ad45ba763d52c06212519eb6fac1
Braden Walker <wwbraden@gmail.com>
2015-07-28 20:19:36 -0500

    starting Edit Event feature

--- client/js/controllers/calendar/calendar.js
+++ client/js/controllers/calendar/calendar.js
@@ -2,22 +2,43 @@ angular.module('roomEase')
 
     .controller('calendarCtrl', function ($scope, Request, $location){
         $scope.calendarView = 'month';
-        $scope.currentDay = new Date();
+        $scope.currentDay = new Date(2015,7,28,15);
 
         $scope.events = [
             {
-                title: 'My event title', // The title of the event
+                title: 'My first event', // The title of the event
                 type: 'info', // The type of the event (determines its color). Can be important, warning, info, inverse, success or special
-                startsAt: new Date(2013,5,1,1), // A javascript date object for when the event starts
-                endsAt: new Date(2014,8,26,15), // Optional - a javascript date object for when the event ends
+                startsAt: new Date(2015,7,27,15), // A javascript date object for when the event starts
+                endsAt: new Date(2015,7,27,15), // Optional - a javascript date object for when the event ends
                 editable: false, // If edit-event-html is set and this field is explicitly set to false then dont make it editable.
                 deletable: false, // If delete-event-html is set and this field is explicitly set to false then dont make it deleteable
-                draggable: true, //Allow an event to be dragged and dropped
-                resizable: true, //Allow an event to be resizable
-                incrementsBadgeTotal: true, //If set to false then will not count towards the badge total amount on the month and year view
+                draggable: false, //Allow an event to be dragged and dropped
+                resizable: false, //Allow an event to be resizable
+                incrementsBadgeTotal: false, //If set to false then will not count towards the badge total amount on the month and year view
+                recursOn: 'year', // If set the event will recur on the given period. Valid values are year or month
+                cssClass: 'a-css-class-name' //A CSS class (or more, just separate with spaces) that will be added to the event when it is displayed on each view. Useful for marking an event as selected / active etc
+            },
+            {
+                title: 'My second event', // The title of the event
+                type: 'info', // The type of the event (determines its color). Can be important, warning, info, inverse, success or special
+                startsAt: new Date(2015,7,28,15), // A javascript date object for when the event starts
+                endsAt: new Date(2015,7,28,15), // Optional - a javascript date object for when the event ends
+                editable: false, // If edit-event-html is set and this field is explicitly set to false then dont make it editable.
+                deletable: false, // If delete-event-html is set and this field is explicitly set to false then dont make it deleteable
+                draggable: false, //Allow an event to be dragged and dropped
+                resizable: false, //Allow an event to be resizable
+                incrementsBadgeTotal: false, //If set to false then will not count towards the badge total amount on the month and year view
                 recursOn: 'year', // If set the event will recur on the given period. Valid values are year or month
                 cssClass: 'a-css-class-name' //A CSS class (or more, just separate with spaces) that will be added to the event when it is displayed on each view. Useful for marking an event as selected / active etc
             }
         ];
+
+        $scope.toggle = function($event, field, event) {
+          $event.preventDefault();
+          $event.stopPropagation();
+          event[field] = !event[field];
+        };
+
+
 
     });
