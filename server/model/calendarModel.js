18fb7a0dcc7dcf595657a84699e8c9d1c2f4714c
Richard Van Camp <richard.vancamp@tut.fi>
2015-07-29 08:56:51 -0500

    new api routes and handlers for calendar

--- server/model/calendarModel.js
+++ server/model/calendarModel.js
@@ -1,9 +1,9 @@
 var db = require('../db/db.js').db;
 var _ = require('underscore');
 
-exports.findEvents = function(houseId, cb) {
+exports.fetchAllEvents = function(houseId, cb) {
 
-}
+};
 
 exports.addEvent = function(event, userId, dwellingId, cb){
 
@@ -13,11 +13,27 @@ exports.addEvent = function(event, userId, dwellingId, cb){
       + "'" + event.title + ", "
       + "'" + event.type + ", "
       + "'" + event.end_at + ", "
-      + "'" + event.userId + ", "
+      + "'" + userId + ", "
       + "'" + dwellingId  + "') RETURNING id;";
 
   db.query(queryString, function(err, results){
     console.log("Saving event to the database");
     err ? cb(err, null) : cb(null, results.rows);
   });
-}
+};
+
+exports.fetchEventsByDwellingId = function(dwellingId, cb){
+
+  var queryString = "SELECT * FROM calendar_events WHERE id = " + dwellingId + ";";
+  db.query(queryString, function(err, results){
+    if(err) {
+      console.log(err);
+    } else {
+      if(!results.rows[0]) {
+        cb("Invalid dwelling ID", null);
+      }else{
+        err ? cb(err, null) : cb(null, results);
+      }
+    }
+  });
+};
