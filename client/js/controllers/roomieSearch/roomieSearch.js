b9ef9cf02ac6a0674ddcf6bbe5017de4c6330bc0
john <redford.john.m@gmail.com>
2015-07-30 10:51:54 -0500

    fix for rommiesearch

--- client/js/controllers/roomieSearch/roomieSearch.js
+++ client/js/controllers/roomieSearch/roomieSearch.js
@@ -1,15 +1,15 @@
 angular.module('roomEase')
 
-.controller('roomieSearchCtrl', function($scope){
-  // $scope.lookingUsers = 
+.controller('roomieSearchCtrl', function ($scope, Request){
+  $scope.lookingUsers = [];
   Request.user.fetchAll().then(function(results){
     console.log("user fetchAll results:", results);
-    users = results;
-    for (var i = 0; i < results.length; i++) {
+    users = results.data;
+    for (var i = 0; i < users.length; i++) {
       $scope.lookingUsers.push({
-        name     : results[i].username;
-        image    : results[i].picture;
-        location : 'placeholder'
+        name     : users[i].username,
+        image    : users[i].picture,
+        location : 'placeholder',
         age      : 0
       })
     }
