debca84e8b9df5bcc5110ae33e336411615d21f7
john <redford.john.m@gmail.com>
2015-07-30 10:06:25 -0500

    for rebase

--- client/js/controllers/roomieSearch/roomieSearch.js
+++ client/js/controllers/roomieSearch/roomieSearch.js
@@ -1,31 +1,7 @@
 angular.module('roomEase')
 
 .controller('roomieSearchCtrl', function($scope){
-  $scope.lookingUsers = [
-    {
-      name: "Hadley",
-      image: "https://pbs.twimg.com/profile_images/598565685548036096/tcsYDtn3_400x400.jpg",
-      location: "Baton Rouge,LA",
-      age: 30
-    },
-    {
-      name: "Abhi",
-      image: "https://avatars0.githubusercontent.com/u/8756983?v=3&s=460",
-      location: "Bloomington,IL",
-      age: 28
-    },
-    {
-      name: "Allen",
-      image: "https://avatars2.githubusercontent.com/u/11891171?v=3&s=400",
-      location: "Omaha,NE",
-      age: 26
-    },
-    {
-      name: "Colin",
-      image: "https://avatars2.githubusercontent.com/u/5582627?v=3&s=400",
-      location: "Austin,TX",
-      age: 22
-    },
-  ]
+  // $scope.lookingUsers = 
+  
 
 })
